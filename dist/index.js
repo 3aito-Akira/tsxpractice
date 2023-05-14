0d8c722695e9365d9aa923814f84bc5d71e2a7ec
akira <yuhstamtch@yahoo.co.jp>
2023-05-14 11:51:40 +0900

    インスタンス生成

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,16 @@
 "use strict";
+var __classPrivateFieldGet = (this && this.__classPrivateFieldGet) || function (receiver, state, kind, f) {
+    if (kind === "a" && !f) throw new TypeError("Private accessor was defined without a getter");
+    if (typeof state === "function" ? receiver !== state || !f : !state.has(receiver)) throw new TypeError("Cannot read private member from an object whose class did not declare it");
+    return kind === "m" ? f : kind === "a" ? f.call(receiver) : f ? f.value : state.get(receiver);
+};
+var __classPrivateFieldSet = (this && this.__classPrivateFieldSet) || function (receiver, state, value, kind, f) {
+    if (kind === "m") throw new TypeError("Private method is not writable");
+    if (kind === "a" && !f) throw new TypeError("Private accessor was defined without a setter");
+    if (typeof state === "function" ? receiver !== state || !f : !state.has(receiver)) throw new TypeError("Cannot write private member to an object whose class did not declare it");
+    return (kind === "a" ? f.call(receiver, value) : f ? f.value = value : state.set(receiver, value)), value;
+};
+var _User4_age;
 const calBMI = ({ height, weight }) => { return weight / height ** 2; };
 const akira = { height: 1.84, weight: 72 };
 console.log(calBMI(akira));
@@ -52,3 +64,68 @@ function map(array, callback) {
 const data = [1, 1, 2, 3, 5, 8, 13];
 const result = map((data), (x) => String(x * 10));
 console.log(result);
+class User {
+    constructor() {
+        this.name = "";
+        this.age = 0;
+    }
+    isAdult() {
+        return this.age > 20;
+    }
+    setAge(newAge) {
+        this.age = newAge;
+    }
+}
+class User3 {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    isAdult() {
+        return this.age > 20;
+    }
+    setAge(newAge) {
+        this.age = newAge;
+    }
+}
+const akira1 = new User();
+console.log(akira1.name);
+console.log(akira1.isAdult());
+console.log(akira1.age);
+akira1.setAge(40);
+console.log(akira1.isAdult());
+console.log(akira1.age);
+const akira3 = new User3("akira", 40);
+console.log(akira3.age);
+class User4 {
+    constructor() {
+        _User4_age.set(this, 0);
+    }
+    getAge() {
+        return __classPrivateFieldGet(this, _User4_age, "f");
+    }
+    setAge(age) {
+        if (age < 0 || age > 150) {
+            return;
+        }
+        __classPrivateFieldSet(this, _User4_age, age, "f");
+    }
+}
+_User4_age = new WeakMap();
+const double1 = (arg) => { console.log(arg * 2); };
+const triple = (arg) => { console.log(arg * 3); };
+double1.isUsed = true;
+triple.isUsed = true;
+console.log("double1");
+double1(100);
+console.log("triple");
+triple(100);
+class User5 {
+    constructor() {
+        this.name = "user5";
+        this.age = 0;
+    }
+}
+const MyUser = User5;
+const u = new MyUser();
+console.log(u.name, u.age);
--- src/index.ts
+++ src/index.ts
@@ -91,4 +91,85 @@ const data = [1,1,2,3,5,8,13];
 const result = map<number,string>((data),(x)=>String(x * 10));
 console.log(result);
 
+class User{
+    name:string = "";
+    age:number = 0;
+
+    isAdult():boolean{
+        return this.age > 20;
+    }
+
+    setAge(newAge:number){
+        this.age = newAge;
+    }
+}
+
+class User3{
+    name:string;
+    age:number;
+
+    constructor(name:string,age:number){
+        this.name = name;
+        this.age = age;
+    }
+
+    isAdult():boolean{
+        return this.age > 20;
+    }
+
+    setAge(newAge:number){
+        this.age = newAge;
+    }
+}
+
+const akira1 = new User();
+console.log(akira1.name);
+console.log(akira1.isAdult());
+console.log(akira1.age);
+akira1.setAge(40);
+console.log(akira1.isAdult());
+console.log(akira1.age);
+
+const akira3 = new User3("akira",40);
+console.log(akira3.age);
+
+class User4{
+    #age:number = 0;
+    getAge(){
+        return this.#age;
+    }
+    setAge(age:number){
+        if(age < 0 || age > 150){
+            return;
+        }
+        this.#age = age;
+    }
+}
+
+type Myfunction = {
+    isUsed:boolean;
+    (arg:number):void;
+}
+
+const double1: Myfunction = (arg:number) => {console.log(arg * 2)};
+const triple:Myfunction = (arg:number) =>{console.log(arg * 3)};
+double1.isUsed = true;
+triple.isUsed = true;
+console.log("double1")
+double1(100);
+console.log("triple");
+triple(100);
+
+
+class User5 {
+    name:string = "user5";
+    age:number = 0;
+}
+
+type MyUserConstructor = new () => User5;
+const MyUser:MyUserConstructor = User5;
+const u = new MyUser();
+console.log(u.name,u.age);
+
+
 
