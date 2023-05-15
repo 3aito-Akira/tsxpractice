f2536762ecb379cd1ba5dc6cccb422737989482d
akira <yuhstamtch@yahoo.co.jp>
2023-05-15 10:41:43 +0900

    closure

--- dist/index.js
+++ dist/index.js
@@ -1,74 +1,16 @@
 "use strict";
-var __classPrivateFieldGet = (this && this.__classPrivateFieldGet) || function (receiver, state, kind, f) {
-    if (kind === "a" && !f) throw new TypeError("Private accessor was defined without a getter");
-    if (typeof state === "function" ? receiver !== state || !f : !state.has(receiver)) throw new TypeError("Cannot read private member from an object whose class did not declare it");
-    return kind === "m" ? f : kind === "a" ? f.call(receiver) : f ? f.value : state.get(receiver);
-};
 var __classPrivateFieldSet = (this && this.__classPrivateFieldSet) || function (receiver, state, value, kind, f) {
     if (kind === "m") throw new TypeError("Private method is not writable");
     if (kind === "a" && !f) throw new TypeError("Private accessor was defined without a setter");
     if (typeof state === "function" ? receiver !== state || !f : !state.has(receiver)) throw new TypeError("Cannot write private member to an object whose class did not declare it");
     return (kind === "a" ? f.call(receiver, value) : f ? f.value = value : state.set(receiver, value)), value;
 };
-var _User4_age, _User_age;
-const getName = (u) => u.name;
-const users1 = [{ name: "akira", age: 26 }, { name: "keiko", age: 15 }];
-const names = users1.map(getName);
-console.log(names);
-console.log(users1.filter((u) => u.age >= 20));
-const xRepeat = (num) => "x".repeat(num);
-const double = (arg) => { console.log(arg * 2); };
-double(100);
-const fromAge = (age) => ({ name: "John Smith", age, });
-const f = fromAge;
-const object2 = f(100);
-console.log(object2);
-function repeat(element, length) {
-    const result = [];
-    for (let i = 0; i < length; i++) {
-        result.push(element);
-    }
-    return result;
-}
-function map(array, callback) {
-    const result = [];
-    for (const elm of array) {
-        result.push(callback(elm));
-    }
-    return result;
-}
-const data = [1, 1, 2, 3, 5, 8, 13];
-const result = map((data), (x) => String(x * 10));
-console.log(result);
-class User3 {
-    constructor(name, age) {
-        this.name = name;
-        this.age = age;
-    }
-    isAdult() {
-        return this.age > 20;
-    }
-    setAge(newAge) {
-        this.age = newAge;
-    }
-}
-const akira3 = new User3("akira", 40);
-console.log(akira3.age);
-class User4 {
-    constructor() {
-        _User4_age.set(this, 0);
-    }
-    getAge() {
-        return __classPrivateFieldGet(this, _User4_age, "f");
-    }
-    setAge(age) {
-        if (age < 0 || age > 150) {
-            return;
-        }
-        __classPrivateFieldSet(this, _User4_age, age, "f");
-    }
-}
-_User4_age = new WeakMap();
+var __classPrivateFieldGet = (this && this.__classPrivateFieldGet) || function (receiver, state, kind, f) {
+    if (kind === "a" && !f) throw new TypeError("Private accessor was defined without a getter");
+    if (typeof state === "function" ? receiver !== state || !f : !state.has(receiver)) throw new TypeError("Cannot read private member from an object whose class did not declare it");
+    return kind === "m" ? f : kind === "a" ? f.call(receiver) : f ? f.value : state.get(receiver);
+};
+var _User_age;
 const double1 = (arg) => { console.log(arg * 2); };
 const triple = (arg) => { console.log(arg * 3); };
 double1.isUsed = true;
@@ -77,6 +19,7 @@ console.log("double1");
 double1(100);
 console.log("triple");
 triple(100);
+//newシグニチャー
 class User5 {
     constructor() {
         this.name = "user5";
@@ -86,26 +29,7 @@ class User5 {
 const MyUser = User5;
 const u = new MyUser();
 console.log(u.name, u.age);
-class User6 {
-    constructor(name, age) {
-        this.name = name;
-        this.age = age;
-    }
-}
-function getPrice(customer) {
-    if (customer instanceof User6) {
-        if (customer.name === "akira") {
-            return 0;
-        }
-    }
-    return customer.age < 18 ? 1000 : 1800;
-}
-const customer1 = { age: 15 };
-const customer2 = { age: 40 };
-const akira2 = new User6("akira", 40);
-console.log(getPrice(customer1));
-console.log(getPrice(customer2));
-console.log(getPrice(akira2));
+//this reference
 class User {
     constructor(name, age) {
         _User_age.set(this, void 0);
@@ -132,10 +56,35 @@ console.log(akira.isAdult === keiko.isAdult);
 console.log(akira.isAdult.apply(bill, []));
 console.log(akira.isAdult.apply(keiko, []));
 console.log(akira.isAdult.apply(flora, []));
-console.log("エラー発生前");
-function throwError() {
-    const error = new Error("エラーを発生させた");
-    throw error;
+function createUser(name, age) {
+    if (name === "") {
+        throw new Error("名前無しのユーザーは作れません");
+    }
+    const user = { name, age };
+    return user;
 }
-throwError();
-console.log("エラー発生後");
+function getMessage(user, message) {
+    return `${user.name} (${user.age}) 「${message}」`;
+}
+const user2Akira = createUser("akira", 40);
+console.log(getMessage(user2Akira, "nice to meet you"));
+class User3 {
+    constructor(name, age) {
+        if (name === "") {
+            throw new Error("名前無しのユーザーは作れません");
+        }
+        this.name = name;
+        this.age = age;
+    }
+    getMessage1(message) {
+        return `${this.name} (${this.age}) 「${message}」`;
+    }
+}
+const user3Akira = new User3("akira", 40);
+console.log(user3Akira.getMessage1("nice to meet you"));
+function createUser1(name, age) {
+    return (message) => { return `${name} (${age}) 「${message}」`; };
+}
+;
+const getMessage2 = createUser1("akira", 40);
+console.log(getMessage2("nice to meet you"));
--- src/index.ts
+++ src/index.ts
@@ -57,12 +57,54 @@ console.log(akira.isAdult.apply(bill,[]));
 console.log(akira.isAdult.apply(keiko,[]));
 console.log(akira.isAdult.apply(flora,[]));
 
+type User2 = {
+    name:string;
+    age:number;
+}
 
+function createUser(name:string,age:number):User2{
+    if(name === ""){
+        throw new Error("名前無しのユーザーは作れません");
+    }
 
+    const user:User2 = {name,age};
+    
+    return user;
+}
 
+function getMessage(user:User2,message:string):string{
+    return `${user.name} (${user.age}) 「${message}」`;
+}
 
+const user2Akira = createUser("akira",40);
+console.log(getMessage(user2Akira,"nice to meet you"));
 
+class User3 {
+    name:string;
+    age:number;
 
+    constructor(name:string,age:number){
+        if(name === ""){
+            throw new Error("名前無しのユーザーは作れません");
+        }
+        this.name = name; this.age = age;
+    }
+
+    public getMessage1(message:string):string{
+        return `${this.name} (${this.age}) 「${message}」`;
+    }
+    
+}
+//closure
+const user3Akira = new User3("akira",40);
+console.log(user3Akira.getMessage1("nice to meet you"));
+
+function createUser1(name:string,age:number){
+    return (message:string) => {return `${name} (${age}) 「${message}」`}
+};
+
+const getMessage2 = createUser1("akira",40);
+console.log(getMessage2("nice to meet you"));
 
 
 
